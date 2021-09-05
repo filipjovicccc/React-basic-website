1331cf247c80eb380c45ba620a78fb70594e54dc
filipjovicccc <filipjovicccc@gmail.com>
2021-09-05 22:57:14 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Logo from "../assets/socer-logo.png"
+// import { BiBasketball } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { BsFillBarChartFill } from "react-icons/bs";
 import "../css/Navbar.css";
@@ -14,6 +15,7 @@ function Navbar() {
         <div className="navbar">
             <div className="leftSide" id={openLinks ? "open" : "close"}>
                 <img src={Logo} />
+                {/* <BiBasketball /> */}
                 <div className="hiddenLinks">
                     <Link to="/"> Home </Link>
                     <Link to="/menu"> Menu </Link>
